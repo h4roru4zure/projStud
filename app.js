9016367b6be79e4c2de93fb7fddcda5333cb3fcb
h4roru <h4roru4zure@outlook.com>
2023-04-19 12:49:42 -0300

    first render of an view  News with the engine EJS

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express =require('express');
 const app =express();
 
 const port=5000;
+//static files
+app.use(express.static('public'));
+app.use('/css',express.static(__dirname +'public/css'));
+app.use('/img',express.static(__dirname +'public/img'));
+app.use('/js',express.static(__dirname +'public/js'));
 
+//template Engine
+app.set('views','./src/views');
+app.set('view engine','ejs');
+
+//Routes 
+const newsRouter =require('./src/routes/news');
+app.use('/',newsRouter);
 
 app.listen(port ,()=>{console.log(`listen in the ${port}`)});
---
+++ src/routes/news.js
@@ -0,0 +1,12 @@
+const express =require('express');
+
+const newsRouter =express.Router();
+
+
+newsRouter.get('',async(req,res)=>{
+    res.render('news');
+});
+
+
+
+module.exports=newsRouter;
